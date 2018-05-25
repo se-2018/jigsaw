5b2d7ddcd6b0c13e1a94594907659ca40cc09a02
LinJinghua <autcaesarautnihil@foxmail.com>
2018-05-25 23:15:00 +0800

    Judge Code

--- src/judge/main.java
+++ src/judge/main.java
@@ -24,16 +24,22 @@ import java.util.Scanner;
  * @author  se-2018
  */
 public class main {
+    private final static long NANOSECOND_PER_MICROSECOND = 1000000L;
+
     private final static int ASTAR_UPPER_LIMIT = 29000;
+    private final static int ASTAR_LOWER_LIMIT = 1000;
+    private final static long ASTAR_LOWER_LIMIT_TIME = 15;
     private final static int ASTAR_TEST_TIME = 3;
 
     private final static int BFS_SCORE = 2;
-    private final static int ASTAR_SCORE = 5;
+    private final static int ASTAR_SCORE = 12;
+    private final static int ASTAR_MAX_SCORE = 14;
 
     private final static JigsawNode DEST_NODE = new JigsawNode(new int[]{25,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,0});
     private static int bfsLength;
     private static JigsawNode bfsNode;
     private static JigsawNode[] aStarNodes = new JigsawNode[ASTAR_TEST_TIME];
+
 
     private static JigsawNode getJigsawNode(Scanner scan) {
         int len = JigsawNode.getDimension() * JigsawNode.getDimension();
@@ -51,29 +57,50 @@ public class main {
         return 0;
     }
 
-    public static int calAStarScore(double searchedNodesNum) {
+    public static int calAStarScore(int rawScore) {
+        if (rawScore > ASTAR_MAX_SCORE * 2) {
+            return ASTAR_MAX_SCORE;
+        }
+        if (rawScore >= ASTAR_MAX_SCORE) {
+            return rawScore % ASTAR_MAX_SCORE
+                + rawScore / ASTAR_MAX_SCORE * (ASTAR_SCORE / ASTAR_TEST_TIME);
+        }
+        return rawScore;
+    }
+
+    public static int estimateAStarScore(int searchedNodesNum, long runTime) {
+        if (searchedNodesNum < ASTAR_LOWER_LIMIT && runTime <= ASTAR_LOWER_LIMIT_TIME) {
+            return ASTAR_MAX_SCORE;
+        }
         if (searchedNodesNum < ASTAR_UPPER_LIMIT) {
-            return ASTAR_SCORE;
+            return ASTAR_SCORE / ASTAR_TEST_TIME;
         }
         return 0;
     }
 
 
-    public static int TestBFS() {
+    public static int TestBFS(String runtimeToken) {
         JigsawNode destNode = DEST_NODE;
         JigsawNode startNode = bfsNode;
 
         Jigsaw solution = new Solution();
+        boolean solved = false;
 
         try {
-            if (!solution.BFSearch(new JigsawNode(startNode), new JigsawNode(destNode))) {
-                return 0;
-            }
+            final long startTime = System.nanoTime();
+
+            solved = solution.BFSearch(new JigsawNode(startNode), new JigsawNode(destNode));
+
+            final long duration = System.nanoTime() - startTime;
+            System.out.println("\nBFS Total " + runtimeToken + ":" + duration / NANOSECOND_PER_MICROSECOND + "ms");
         } catch (Throwable th) {
             th.printStackTrace();
-            return 0;
+            System.exit(0);
         }
 
+        if (!solved) {
+            return 0;
+        }
         List<JigsawNode> solutionPath = solution.getPath();
         if (!Jigsaw.isValidPath(solutionPath, startNode, destNode)) {
             return 0;
@@ -84,18 +111,23 @@ public class main {
 
     public static int TestAStar(String runtimeToken) {
         long totalTime = 0;
-        double searchedNodesNum = 0;
+        int rawScore = 0;
+
         for (int i = 0; i < ASTAR_TEST_TIME; i++) {
             final long startTime = System.nanoTime();
-            searchedNodesNum += TestAStar(aStarNodes[i], DEST_NODE);
-            final long duration = System.nanoTime() - startTime;
-            totalTime += duration;
-            System.out.println("\n" + runtimeToken + ":" + duration / 1000000 + "ms");
-        }
-        System.out.println("\nTotal " + runtimeToken + ":" + totalTime / 1000000 + "ms");
-        System.out.println("Average " + runtimeToken + ":" + totalTime / 1000000 / ASTAR_TEST_TIME + "ms");
 
-        return calAStarScore(searchedNodesNum / ASTAR_TEST_TIME);
+            int searchedNodesNum = TestAStar(aStarNodes[i], DEST_NODE);
+
+            final long duration = System.nanoTime() - startTime;
+            System.out.println("\nAStar " + runtimeToken + ":" + duration / NANOSECOND_PER_MICROSECOND + "ms");
+            totalTime += duration;
+
+            rawScore += estimateAStarScore(searchedNodesNum, duration / NANOSECOND_PER_MICROSECOND);
+        }
+
+        System.out.println("\nAStar Total " + runtimeToken + ":" + totalTime / NANOSECOND_PER_MICROSECOND + "ms");
+
+        return calAStarScore(rawScore);
     }
 
     public static int TestAStar(JigsawNode startNode, JigsawNode destNode) {
@@ -107,7 +139,7 @@ public class main {
             }
         } catch (Throwable th) {
             th.printStackTrace();
-            return Integer.MAX_VALUE;
+            System.exit(0);
         }
 
         List<JigsawNode> solutionPath = solution.getPath();
@@ -133,6 +165,6 @@ public class main {
             aStarNodes[i] = getJigsawNode(scan);
         }
 
-        System.out.println("\n" + scoreToken + ":" + (TestBFS() + TestAStar(runtimeToken)));
+        System.out.println("\n" + scoreToken + ":" + (TestBFS(runtimeToken) + TestAStar(runtimeToken)));
     }
 }
